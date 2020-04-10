9cdeef4c329fabec3e20961852d46207a08917d2
NastiiaV <awerga13@gmail.com>
2020-04-10 13:39:53 +0300

    deleted some

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -19,35 +19,7 @@ namespace AutoTest
             driver.Navigate().GoToUrl(PageObjectBase.url);
 
         }
-
-        //[OneTimeTearDown]
-        //public void OneTimeTearDown() => driver.Quit();
-
-        ////[TestCase(true, "dress")]
-        //[TestCase(false, "asd")]
-        //public void Search(bool isPositive, string someSearch)
-        //{
-        //    SearchPage searchPage = new SearchPage(driver);
-        //    bool isSearchExist = searchPage.EnterData(someSearch).isSearchOk();
-        //    Assert.That(isSearchExist, Is.EqualTo(isPositive), $"Search is {(isSearchExist ? "existed" : "not existed")} " +
-        //        "but we expected opposite");
-        //}
-
-        //[TestCase(true, "test@gmail.com","1234")]
-        //[TestCase(false, "(test@gmail.com","12345")]
-
-        //public void SignInValidation(bool isPositive, string email,string password)
-        //{
-
-        //    Header header = new Header(driver);
-        //    SignInPage signInPage = header.ClickOnSignIn();
-        //    bool isDataOk = signInPage.EnterData(email).IsDataOk(password);
-
-        //    Assert.That(isDataOk,
-        //        Is.EqualTo(isPositive), $"Email was validated {(isDataOk ? "successfully" : "unseccessfully")} " +
-        //        "but we expected opposite");
-
-        //}
+        
 
         
     }
